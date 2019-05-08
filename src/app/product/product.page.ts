a5b09d0770df5c41bef33e53c8d511f5cb4bbe0a
JasmeenBong <jasmeenbky@gmail.com>
2019-05-09 00:47:50 +0800

    Update product.page.ts

--- src/app/product/product.page.ts
+++ src/app/product/product.page.ts
@@ -181,10 +181,7 @@ export class ProductPage implements OnInit {
         this.router.navigate(['/categories']);
     }
 
-    ngOnInit() {}
-
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   chat(){
       this.router.navigate(['/chatbox']);
