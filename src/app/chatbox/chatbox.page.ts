a45137c20e4fc0b1e1ca2e49372f31418c328ecc
JasmeenBong <jasmeenbky@gmail.com>
2019-05-16 11:41:31 +0800

    categories page
    - fix the get user problem
    - fix the bug in the chatbox
    - fix the bug in the inbox
    - solve the product page
    - add badge in the inbox tab

--- src/app/chatbox/chatbox.page.ts
+++ src/app/chatbox/chatbox.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import * as firebase from 'firebase/app';
 import * as angulardb from 'angularfire2/database';
+import { DatePipe } from '@angular/common';
 
 
 @Component({
@@ -20,42 +21,45 @@ export class ChatboxPage implements OnInit {
   msgRef;
   sendEnabled = false;
 
-  constructor(private route:ActivatedRoute, public db: angulardb.AngularFireDatabase) { 
-    this.reciever = this.route.snapshot.paramMap.get('reciever');
-    this.sender = this.route.snapshot.paramMap.get('sender');
-    this.retMsg = firebase.database().ref('messages');
-    console.log(this.reciever + ' '+this.sender);
-    this.subs = this.db.list('/messages').valueChanges().subscribe( data=>{
+  constructor(private route:ActivatedRoute, public db: angulardb.AngularFireDatabase, private datePipe :DatePipe) {
+  this.showMessage();
+  }
 
-      this.retMsg.on("value",(snapshot)=>{
-         snapshot.forEach((childSnapshot)=> {
-           if(this.sender == childSnapshot.val().sender && this.reciever == childSnapshot.val().reciever){
-            this.getMsgs = data;
-          }
-          });
-    });
-  });
-}
+  showMessage(){
+  this.reciever = this.route.snapshot.paramMap.get('reciever');
+  this.sender = this.route.snapshot.paramMap.get('sender');
+  this.retMsg = firebase.database().ref('messages');
+  this.retMsg.on("value",(snapshot)=>{
+        this.getMsgs = [];
+        snapshot.forEach((childSnapshot)=> {
+         if((this.sender == childSnapshot.val().sender && this.reciever == childSnapshot.val().reciever) || (this.reciever == childSnapshot.val().sender && this.sender == childSnapshot.val().reciever)){
+           this.getMsgs.push(childSnapshot.val());
+        }
+        });
+      });
+  }
 
-  
- 
+
+
   send(){
     this.msg = firebase.database().ref('messages');
     this.msgRef = this.msg.child('/').push({
       sender: this.sender,
       reciever: this.reciever,
-      message: this.message
+      message: this.message,
+      timeStamp: this.datePipe.transform(new Date(), 'yyyy-MM-dd hh:mm:ss'),
+      status: 'unread'
     })
-
     this.message = "";
   }
+
   ionViewdidLoad(){
-    
+
   }
 
   ngOnInit() {
 
-  
+
    }
 
-}       
+}
